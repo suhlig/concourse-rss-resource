ed2ee5799bd38b1bd96fce6ba7e2014542eeaa3e
Steffen Uhlig,ibm <Steffen.Uhlig@de.ibm.com>
2016-11-02 14:52:17 +0100

    Run all specs after a shared example changed

--- spec/shared/check_spec.rb
+++ spec/shared/check_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 require 'spec_helper'
 
-shared_examples 'check' do |parameter|
+shared_examples 'check' do
   it 'produces the right JSON output' do
     pipe_in_file('../../spec/fixtures/check/input.json')
     close_input
